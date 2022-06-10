78a82e591e6bec11c469d9731030efd5371fa867
Alejandro Ponce <alexponce36@hotmail.com>
2022-06-09 20:33:32 -0500

    feat: dynamic rabbitmq settings

--- MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -2,6 +2,7 @@
 using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Domain.Core.Commands;
 using MicroRabbit.Domain.Core.Events;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
 using System.Text;
@@ -13,21 +14,23 @@ namespace MicroRabbit.Infra.Bus
         private readonly IMediator _mediator;
         private readonly Dictionary<string, List<Type>> _handlers;
         private readonly List<Type> _eventTypes;
+        private RabbitMQSettings _rabbitMQSettings;
 
-        public RabbitMQBus(IMediator mediator)
+        public RabbitMQBus(IMediator mediator, IOptions<RabbitMQSettings> rabbitMQSettings)
         {
             _mediator = mediator;
             _handlers = new Dictionary<string, List<Type>>();
             _eventTypes = new List<Type>();
+            _rabbitMQSettings = rabbitMQSettings.Value;
         }
 
         public void Publish<T>(T @event) where T : Event
         {
             var connectionFactory = new ConnectionFactory()
             {
-                HostName = "localhost",
-                UserName = "alejandro",
-                Password = "5r0sc0Ra"
+                HostName = _rabbitMQSettings.Hostname,
+                UserName = _rabbitMQSettings.Username,
+                Password = _rabbitMQSettings.Password
             };
 
             using(var connection = connectionFactory.CreateConnection())
