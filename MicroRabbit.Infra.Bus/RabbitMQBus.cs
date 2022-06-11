83402cf89e3175dbf09e718513b6d759f59b0f2f
Alejandro Ponce <alexponce36@hotmail.com>
2022-06-11 13:55:41 -0500

    feat: subscribe messages in infraestructure Bus

--- MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -5,6 +5,7 @@ using MicroRabbit.Domain.Core.Events;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 namespace MicroRabbit.Infra.Bus
@@ -55,7 +56,83 @@ namespace MicroRabbit.Infra.Bus
             where T : Event
             where TH : IEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = typeof(T).Name;
+            var handlerType = typeof(TH);
+
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+
+            if (!_handlers.ContainsKey(eventName))
+            {
+                _handlers.Add(eventName, new List<Type>());
+            }
+
+            if (_handlers[eventName].Any(s => s.GetType() == handlerType))
+            {
+                throw new ArgumentException($"El handler exception {handlerType.Name} ya fue registrado anteriormente por '{eventName}'", nameof(handlerType));
+            }
+
+            _handlers[eventName].Add(handlerType);
+
+            StartBasicConsume<T>();
+        }
+
+        private void StartBasicConsume<T>() where T : Event
+        {
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = _rabbitMQSettings.Hostname,
+                UserName = _rabbitMQSettings.Username,
+                Password = _rabbitMQSettings.Password,
+                DispatchConsumersAsync = true
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            var eventName = typeof(T).Name;
+            channel.QueueDeclare(eventName, false, false, false, null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.Received += Consumer_Recieved;
+            channel.BasicConsume(eventName, true, consumer);
+        }
+
+        private async Task Consumer_Recieved(object sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            var message = Encoding.UTF8.GetString(e.Body.Span);
+
+            try
+            {
+                await ProcessEvent(eventName, message).ConfigureAwait(false);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private async Task ProcessEvent(string eventName, string message)
+        {
+            if (_handlers.ContainsKey(eventName))
+            {
+                var subscriptions = _handlers[eventName];
+
+                foreach(var subsciption in subscriptions)
+                {
+                    var handler = Activator.CreateInstance(subsciption);
+                    if (handler == null) continue;
+
+                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                    var @event = JsonConvert.DeserializeObject(message, eventType);
+                    var concretType = typeof(IEventHandler<>).MakeGenericType(eventType);
+
+                    await (Task)concretType.GetMethod("Hanlder").Invoke(handler, new object[] {@event});
+                }
+            }
         }
     }
 }
