c5247ac6f6fe51ce248470f4ea2b47b5f59f2e7c
Alejandro Ponce <alexponce36@hotmail.com>
2022-08-17 21:19:25 -0500

    feat: configuraciones de api

--- MicroRabbit.Transfer.Api/Program.cs
+++ MicroRabbit.Transfer.Api/Program.cs
@@ -1,6 +1,24 @@
+using MicroRabbit.Infra.Bus;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TransferDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TransferDbConnection"))
+);
+
+builder.Services.Configure<RabbitMQSettings>(builder.Configuration.GetSection("RabbitMQSettings"));
+builder.Services.RegisterServices(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    );
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +37,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
